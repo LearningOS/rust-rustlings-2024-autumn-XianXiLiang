91e30eb9bd338cd17085225a312f61b58176adb8
XianXiliang <1272198199@qq.com>
2024-10-19 11:04:20 +0800

    solve algorithm before 7

--- exercises/algorithm/algorithm2.rs
+++ exercises/algorithm/algorithm2.rs
@@ -2,11 +2,10 @@
 	double linked list reverse
 	This problem requires you to reverse a doubly linked list
 */
-// I AM NOT DONE
 
 use std::fmt::{self, Display, Formatter};
 use std::ptr::NonNull;
-use std::vec::*;
+// use std::vec::*;
 
 #[derive(Debug)]
 struct Node<T> {
@@ -74,6 +73,18 @@ impl<T> LinkedList<T> {
     }
 	pub fn reverse(&mut self){
 		// TODO
+        let mut current = self.start;
+        self.start = self.end;
+        self.end = current;
+
+        while let Some(mut node) = current {
+            unsafe {
+                let next = (*node.as_ptr()).next;
+                (*node.as_ptr()).next = (*node.as_ptr()).prev;
+                (*node.as_ptr()).prev = next;
+                current = next;
+            }
+        }
 	}
 }
 
